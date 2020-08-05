166f7eb8a511411dd6e7fc37fafa9e6434bf137c
baptixta <daviarcosbaptista@gmail.com>
2020-08-05 20:53:23 -0300

    Next Level Week // Dia 05/08 - script criado

---
+++ scripts/addField.js
@@ -0,0 +1,30 @@
+// Search the button
+let btnTime = document.querySelector('#add-time')
+btnTime.addEventListener('click', cloneField)
+// When clicking the button
+
+// Executing the action
+function cloneField() {
+    // Duplicating fields
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    // Cleaning inputs
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    // Cleaning each field
+    
+    /* For Loop version 
+    for (let i = 0; i < fields.length; i++) {
+        fields[i].value = ""
+    } */
+    
+    // ForEach version
+    fields.forEach(field => {
+        field.value = ""
+    })
+
+    // Inserting into page
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+}
+
+
