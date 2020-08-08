f3119f0e5b844abcd4325441ec192da8a19253d1
baptixta <daviarcosbaptista@gmail.com>
2020-08-08 01:01:21 -0300

    Next Level Week // Dia 08/08 Nunjucks e Node

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-const profs = [
+// Dados
+const proffys = [
     {
         name: "Diego Fernandes",
         avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
@@ -14,11 +15,11 @@ const profs = [
     },
     {
         name: "Davi Baptista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        avatar: "https://avatars3.githubusercontent.com/u/30696945?s=460&u=b75de85f4eb8b10ee7f6ac03bddb85f695091cff&v=4",
         whatsapp: "976762121",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
+        subject: "Artes",
+        cost: "69",
         weekday: [
             1
         ],
@@ -27,22 +28,69 @@ const profs = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sábado"
+]
+
+// Funcionalidades
+function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+}
+
 function pageLanding(req, res) {
     return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.render("study.html")
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+
+    //Se tiver dados
+    const isNotEmpty = Object.keys(data).length > 0
+    if (isNotEmpty) {
+        data.subject = getSubject(data.subject)
+
+        //Adicionar dados a lista de proffys
+        proffys.push(data)
+
+        return res.redirect("/study")
+    }
+
+    //Se não, mostrar a página  
+
+    return res.render("give-classes.html", { subjects, weekdays })
 }
 
+
+// Servidor
 const express = require('express')
 const server = express()
 
-// Configurar Nunjucks
+// Configurar Nunjucks (template engine)
 const nunjucks = require('nunjucks')
 nunjucks.configure('src/views', {
     express: server,
@@ -50,6 +98,7 @@ nunjucks.configure('src/views', {
 })
 
 
+// Início e config do server
 server
 // Configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
